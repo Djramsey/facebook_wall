125172c5d6b4da4883dbc2a7d32cf2bc6c70fbd1
djramsey <ramses.geronimo@uasdc.org>
2016-03-15 20:45:30 +0000

    initial commit

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -6,8 +6,15 @@ $(document).ready(function()
 	
 	// 1. Make a div for the "wall post" to go in in your HTML.
 	
-	// 2. Make the text in the textbox appear in your div in html when the user clicks the post button.
+	var x = $("#message").text();
+
+$("#post-btn").click(function(){    
 	
+ 	$("#wall").text(x);
+});
+
+	// 2. Make the text in the textbox appear in your div in html when the user clicks the post button.
+
 	// 3. Use an if statement to only "post" if the user has actually entered text.
 	
 	// 4. Add a picture to all of the posts. 
